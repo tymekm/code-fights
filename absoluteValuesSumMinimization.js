4185c4856390f7ed820896fa6d7e19f78c6de3d2
tymekm <tymek.m@hotmail.com>
2018-07-01 07:52:41 +0200

    modified:   absoluteValuesSumMinimization.js

--- absoluteValuesSumMinimization.js
+++ absoluteValuesSumMinimization.js
@@ -26,24 +26,19 @@
 //
 
 function absoluteValuesSumMinimization(a) {
-	var abs = a.reduce((a,b) => a + b);
-//	console.log(abs);
-	var resultArr = [];
-	var result;
-	for(let i = 0; i < a.length; i++){
-		result = 0;
-		for(let j = 0; j < a.length; j++){
-			result += abs*(a[j] - a[i]);
-	console.log(result + " on i= " + i + " and j = " + j);
-			if(j == a.length - 1){
-				result += abs*(a[a.length -1] - a[i]);
-			}
-		}
-		resultArr.push(result);
+	const isEven = a.length % 2 == 0;
+
+	if(isEven == true){
+		console.log(a[a.length /2 - 1]);
+		return(a[a.length /2 - 1]);
+	}else{
+		console.log(a[Math.floor(a.length / 2)]);
+		return(a[Math.floor(a.length / 2)]);
 	}
-	console.log(resultArr);
 }
 
-//absoluteValuesSumMinimization([23]); //23
-//absoluteValuesSumMinimization([0, 7, 9]); // 7 
+absoluteValuesSumMinimization([23]); //23
+absoluteValuesSumMinimization([0, 7, 9]); // 7 
 absoluteValuesSumMinimization([2,4,7]); // 4
+absoluteValuesSumMinimization([-10, -10, -10, -10, -10, -9, -9, -9, -8, -8, -7, -6, -5, -4, -3, -2, -1, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]); // 15
+absoluteValuesSumMinimization([-1000000, -10000, -10000, -1000, -100, -10, -1, 0, 1, 10, 100, 1000, 10000, 100000, 1000000]); // 0
